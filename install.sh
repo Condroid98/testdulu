b285efeaae1ed011e6fc99eae96e3bdf64c37652
Heri Maulana <droidmx98@gmail.com>
2020-12-20 21:54:27 -0800

    perbarui

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "install webserver"
 echo "================================"
 echo -e "Install apache2\n"
-apt–get install -y apache2
+apt-get install -y apache2
 echo -e "\ninstall php & php-mysql\n"
 apt-get install -y php php-mysql
 echo -e "\ninstall mysql-server\n"
