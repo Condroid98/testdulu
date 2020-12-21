b2ea7894fc575aa5b9906d8c2ee2f11985936356
Heri Maulana <droidmx98@gmail.com>
2020-12-20 20:48:33 -0800

    oke

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo -e "Install apache2\n"
 apt–get install -y apache2
 echo -e "\ninstall php & php-mysql\n"
 apt-get install -y php php-mysql
-echo -e "\ninstall mysql\n"
-apt-get install -y mysql
+echo -e "\ninstall mysql-server\n"
+apt-get install -y mysql-server
 echo -e "\ninstall selesai\n"
