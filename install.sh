3ac7a655f21b40ad9d09aaaefa202b77259d30d8
Heri Maulana <droidmx98@gmail.com>
2020-12-20 20:01:10 -0800

    script install webserver

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "install webserver"
+echo "=============================="
+echo "Install apache2"
+sudo apt–get install -y apache2
+echo "install php & php-mysql"
+sudo apt-get install -y php php-mysql
+echo "install mysql"
+sudo apt-get install -y mysql
+echo "install selesai"
